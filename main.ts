09830b2bab80f4a080f85d48b6db02063eacf046
alemoqui <130452678+alemoqui@users.noreply.github.com>
2023-04-25 11:47:19 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,32 +1,121 @@
-let tool = 0
-input.onGesture(Gesture.Shake, function () {
-    tool = randint(0, 2)
-    if (tool == 0) {
+radio.onReceivedNumber(function (receivedNumber) {
+    if (numeros == 1 && receivedNumber == 1) {
         basic.showLeds(`
-            . . # . .
             . # # # .
-            # # # # #
+            . # . . .
+            . # # . .
+            . # . . .
             . # # # .
-            . . # . .
             `)
-    } else if (tool == 1) {
+    }
+    if (numeros == 2 && receivedNumber == 2) {
         basic.showLeds(`
-            # # # # #
-            # # # # #
-            # # # # #
-            # # # # #
-            # # # # #
+            . # # # .
+            . # . . .
+            . # # . .
+            . # . . .
+            . # # # .
             `)
-    } else {
+    }
+    if (numeros == 3 && receivedNumber == 3) {
         basic.showLeds(`
-            # # . . #
-            # # . # .
-            . . # . .
-            # # . # .
-            # # . . #
+            . # # # .
+            . # . . .
+            . # # . .
+            . # . . .
+            . # # # .
+            `)
+    }
+    if (numeros == 1 && receivedNumber == 3) {
+        basic.showLeds(`
+            . # # # .
+            # . . . .
+            # . # # .
+            # . . # .
+            . # # # .
+            `)
+    }
+    if (numeros == 1 && receivedNumber == 2) {
+        basic.showLeds(`
+            . # # # .
+            . # . . #
+            . # . . #
+            . # # # .
+            . # . . .
+            `)
+    }
+    if (numeros == 2 && receivedNumber == 1) {
+        basic.showLeds(`
+            . # # # .
+            # . . . .
+            # . # # .
+            # . . # .
+            . # # # .
+            `)
+    }
+    if (numeros == 2 && receivedNumber == 3) {
+        basic.showLeds(`
+            . # # # .
+            . # . . #
+            . # . . #
+            . # # # .
+            . # . . .
+            `)
+    }
+    if (numeros == 3 && receivedNumber == 1) {
+        basic.showLeds(`
+            . # # # .
+            . # . . #
+            . # . . #
+            . # # # .
+            . # . . .
+            `)
+    }
+    if (numeros == 3 && receivedNumber == 2) {
+        basic.showLeds(`
+            . # # # .
+            # . . . .
+            # . # # .
+            # . . # .
+            . # # # .
             `)
     }
 })
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        # # # # #
+        . # # # .
+        . . # . .
+        `)
+    radio.sendNumber(1)
+    numeros = 1
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showLeds(`
+        # # . . #
+        # # . # .
+        . . # . .
+        # # . # .
+        # # . . #
+        `)
+    radio.sendNumber(3)
+    numeros = 3
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+    radio.sendNumber(2)
+    numeros = 2
+})
+let numeros = 0
+radio.setGroup(10203040506070808)
 basic.forever(function () {
 	
 })
